921712d0fbac843be34320bee4abef6b006acfac
Espadesa Retail S.L <39517033+EspadesaRetail@users.noreply.github.com>
2019-07-12 17:16:46 +0200

    WIP survey - Delete Modules
    Delete Modules that we have not working

--- website_sale_product_pricelist/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import inherited_product_pricelist
-from . import inherited_product_product
-from . import inherited_product_template
-from . import inherited_res_partner
-from . import website_sale_product_pricelist
