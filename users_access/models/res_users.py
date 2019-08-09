8a1d58590bd816c4817b87e9305b62665dc9bebf
EspadesaRetail <alberto.calvo@beds.es>
2019-08-09 13:17:53 +0200

    [WIP] users_access module, add group by to search view on Teams

--- users_access/models/res_users.py
+++ users_access/models/res_users.py
@@ -2,26 +2,6 @@
 from odoo import models, fields, api, _
 
 
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    team_id = fields.Selection(
-        selection='_list_all_teams',
-        string='User Team')
-    shop_role_ids = fields.Many2many(
-        comodel_name='res.users.shop_role',
-        string='Shop Role')
-    role_name = fields.Char(
-        string='Role Name',
-        related='shop_role_ids.name',
-        store=True)
-
-    @api.model
-    def _list_all_teams(self):
-        self._cr.execute("SELECT team, name FROM res_users_team ORDER BY team")
-        return self._cr.fetchall()
-
-
 class ResUsersTeam(models.Model):
     _name = 'res.users.team'
 
@@ -59,3 +39,23 @@ class ResUsersShopRole(models.Model):
     user_ids = fields.Many2many(
         comodel_name='res.users',
         string='Users')
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    team_id = fields.Selection(
+        selection='_list_all_teams',
+        string='User Team')
+    shop_role_ids = fields.Many2many(
+        comodel_name='res.users.shop_role',
+        string='Shop Role')
+    role_name = fields.Char(
+        string='Role Name',
+        related='shop_role_ids.name',
+        store=True)
+
+    @api.model
+    def _list_all_teams(self):
+        self._cr.execute("SELECT team, name FROM res_users_team ORDER BY team")
+        return self._cr.fetchall()
