213804f5ac3df2f7f8bae05034651cad2e7ce090
EspadesaRetail <alberto.calvo@beds.es>
2019-08-09 13:11:34 +0200

    [WIP] users_access module, add group by to search view on Teams

--- users_access/models/res_users.py
+++ users_access/models/res_users.py
@@ -11,6 +11,10 @@ class ResUsers(models.Model):
     shop_role_ids = fields.Many2many(
         comodel_name='res.users.shop_role',
         string='Shop Role')
+    role_name = fields.Char(
+        string='Role Name',
+        related='shop_role_ids.name',
+        store=True)
 
     @api.model
     def _list_all_teams(self):
